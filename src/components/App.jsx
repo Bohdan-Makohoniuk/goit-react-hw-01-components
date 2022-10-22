7497212d1089b99c00b28baf4c0849841f47f3a7
Bohdan <makohoniuk.bo@gmail.com>
2022-10-22 12:18:41 +0300

    Додає всю розмітку на сторінку

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import  Profile from './Profile/Profile'
 import  Statistics from './Statistics/Statistics'
 import  FriendList from './FriendList/FriendList'
+import  TransactionHistory from './TransactionHistory/TransactionHistory'
 import user from './data/user.json'
 import data from './data/data.json'
 import friends from './data/friends.json'
+import transactions from './data/transactions.json'
 
 export function App () {
   return (
@@ -22,6 +24,8 @@ export function App () {
       <Statistics stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />;
       </div>
       ); 
 }
